5488e68ad3fd4e2285ee42c4c634aad221759c50
J0udi18 <101606075+J0udi18@users.noreply.github.com>
2023-03-08 10:05:34 +1300

    Add files via upload

--- Guess_The number.2.py
+++ Guess_The number.2.py
@@ -1,20 +1,40 @@
 # This is a High Low Game
 from random import randint
-guesses = 1
-number = randint(1, 100)
+secret = 7
+guesses_allowed = 5
 
-guess = int(input("Guess a number between 1 and 100."))
+already_guessed = []
+guesses_left = guesses_allowed
+num_won = 0
 
-while guess != number:
-    if guess < number:
+guess = ""
+
+while guess != secret and guesses_left >= 1:
+
+    guess = int(input("guess: "))  # replace this with function
+
+    # check that guess is not a duplicate
+    if guess in already_guessed:
+        print('You already guessed that number! please try again '
+              'you *still* have {} guesses left'.format(guesses_left)
+        continue
+    guesses_left -= 1
+    already_guessed.append(guess)
+
+    if guesses_left : continue
+
+while guess != secret:
+    if guess < secret:
         print("Your guess was too low.")
         guess = int(input("Guess again."))
         guesses = guesses + 1
-    elif guess > number:
+    elif guess > secret:
         print("Your guess was too high.")
         guess = int(input("Guess again."))
         guesses = guesses + 1
 
+if guess == secret:
+    if guesses_left == guesses_allowed: int = 5
 print()
 print("*****Congratulation, you guessed the number!*****")
 prize_decoration = "*"
--- Guess_the number.py
+++ Guess_the number.py
@@ -1,13 +1,15 @@
 import random
 
 number = random.randint(1, 100)
+print("spoiler alert: ", number)
+print()
 print("I'm thinking of a number between 1 and 100. ")
-guessesTaken = 0
+guesses_taken = 0
 
-while guessesTaken < 5:
+while guesses_taken < 5:
     guess = input("Enter a guess: ")
     guess = int(guess)
-    guessesTaken = guessesTaken + 1
+    guesses_taken = guesses_taken + 1
     if guess < number:
         print("That was too low.")
     elif guess > number:
@@ -19,3 +21,4 @@ if guess == number:
     print("You guessed the correct number!")
 else:
     print("You lose, better luck next time.The right number was", number)
+    print()
