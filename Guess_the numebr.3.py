5fde1c9c266a4898cfe65fdf28745e8fbaa080f9
J0udi18 <101606075+J0udi18@users.noreply.github.com>
2023-03-17 13:34:59 +1300

    Add files via upload

--- Guess_the numebr.3.py
+++ Guess_the numebr.3.py
@@ -41,4 +41,34 @@ print("*****Congratulation, you guessed the number!*****")
 prize_decoration = "*"
 print("It only took you", guesses, "guesses!")
 
+# hard coded for testing purposes - in actual game
+# start with empty list and add number of guesses taken
+# to it at the end of each round
+game_scores = [3, 1, 6, 4, 5, 3]
+
+# Get Stats (goes once game is over)
+print("Before sorting...")
+print(game_scores)
+
+game_scores.sort()
+
+print("after sorting...")
+print(game_scores)
+
+# best score is first item in list (lowest)
+best = game_scores[0]
+
+# worst score is last item in list!
+worst = game_scores[-1]
+
+# get average
+total_scores = sum(game_scores)
+average_score = total_scores / len(game_scores)
+
+print()
+print("Stats!!")
+print(f"Best: {best}")
+print(f'Worst: {worst}')
+print(f'Average: {average_score:.2f}')
+
 
