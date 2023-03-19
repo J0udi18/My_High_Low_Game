2b0e41cf782975ce3a80a46f1231228d8d4e9855
J0udi18 <101606075+J0udi18@users.noreply.github.com>
2023-03-20 11:56:02 +1300

    Add files via upload

---
+++ HL_ Average.py
@@ -0,0 +1,4 @@
+""" Get average result """
+def average_result(input_tuple_list, index):
+
+
---
+++ High_Low_Game.v1.py
@@ -0,0 +1,19 @@
+import random
+
+gusses = []
+cpu_num = random.randint(1, 100)
+player_num = int(input("Enter a number between 1-100: "))
+gusses.append(player_num)
+
+while player_num != cpu_num:
+    if player_num > cpu_num:
+        print("Too high!")
+    else:
+        print("Too low")
+    player_num = int(input("Enter a number between 1-100: "))
+    gusses.append(player_num)
+else: 
+    print("well done!")
+    print("It took you i% guesses." % len(gusses))
+    print("Here are your guesses: ")
+    print(gusses)
