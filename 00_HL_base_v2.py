d4ac6d522f77bac1ae32dfaded99e890faabb55a
J0udi18 <101606075+J0udi18@users.noreply.github.com>
2023-03-28 10:37:40 +1300

    Add files via upload

--- 00_HL_base_v2.py
+++ 00_HL_base_v2.py
@@ -90,6 +90,7 @@ def int_checker(question, low=None, high=None, exit_code=None):
 
 rounds_played = 0
 end_game = "no"
+feedback = ""   # initialise feedback to "" so it is never undefined
 
 game_scores = []
 game_history = []
@@ -105,6 +106,12 @@ if played_before == "no":
 
 # Ask user for # of rounds, <enter> for infinite mode
 rounds = int_checker("How many rounds: ", low=0, exit_code="")
+print()
+low_num = int_checker("Please enter a low number: ")
+high_num = int_checker("Please enter a high number: ", low_num)
+
+guess_range = high_num - low_num
+guesses_allowed = 3
 
 print()
 if rounds == "":
@@ -112,6 +119,8 @@ if rounds == "":
     rounds = 5
 
 while rounds_played < rounds and end_game == "no":
+
+    print()
 
     if mode == "infinite":
         heading = f"Continuous Mode: Round {rounds_played + 1}"
@@ -123,12 +132,13 @@ while rounds_played < rounds and end_game == "no":
     print(heading)
 
     num_guesses = 0
-    secret = random.randrange(1, 100)
+    secret = random.randrange(low_num, high_num)
     print(f'Spoiler alert {secret}')
 
     choose = ""
-    while choose != secret:
-        choose = int_checker("Guess:", 1, 100, "xxx")
+    lose = "no"
+    while choose != secret and lose == "no":
+        choose = int_checker("Guess:", low_num, high_num, "xxx")
         if choose == "xxx":
             end_game = "yes"
             break
@@ -142,6 +152,13 @@ while rounds_played < rounds and end_game == "no":
             feedback = f"You got it in {num_guesses + 1}"
 
         num_guesses += 1
+
+        if num_guesses >= guesses_allowed:
+            feedback = f"Sorry you lose, you have used {num_guesses} guesses"
+            print(feedback)
+            lose = "yes"
+
+        print()
 
     outcome = f'Round {rounds_played + 1}: {feedback}'
     game_history.append(outcome)
@@ -161,16 +178,18 @@ worst_score = game_scores[-1]
 
 average = sum(game_scores) / len(game_scores)
 
+print()
+print("**** Game Stats *****")
+
 print(f'Best: {best_score}')
+print(f'Worst: {worst_score}')
 print(f'Average: {average:.2f}')
+
+print()
 
 print(f'**** Game History ******')
 for item in game_history:
     print(item)
 
-print("we are done")
+print("Thank you for playing 😊")
 
-play_again = input("Play again? (y/n): ")
-print()
-if play_again.lower() != "y":
-    "break"
